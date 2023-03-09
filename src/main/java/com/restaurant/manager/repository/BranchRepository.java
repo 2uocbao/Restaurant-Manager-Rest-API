2a98be02e65e36bcbda6621668037fb0efbf0a0b
2uocbao <2uocbao.IT@gmail.com>
2023-03-09 16:55:41 +0700

    Update BranchRepository.java
    Update return datatypes

--- src/main/java/com/restaurant/manager/repository/BranchRepository.java
+++ src/main/java/com/restaurant/manager/repository/BranchRepository.java
@@ -6,19 +6,19 @@ import com.restaurant.manager.model.Branch;
 
 public interface BranchRepository {
 
-	public boolean createBranch(Branch branch);
+	public Branch createBranch(Branch branch);
 
 	public Branch detailBranch(int id);
 
-	public boolean updateBranch(Branch branch);
+	public Branch updateBranch(Branch branch);
 
-	public boolean changeStatusBranch(int id, int status);
+	public Branch changeStatusBranch(int id, int status);
 
 	public Branch getDetailByPhone(String phone);
 
 	public Integer getStatusbyId(int id);
 
-	public boolean changeStatusbyRestaurantId(int restaurantId, int status);
+	public Branch changeStatusbyRestaurantId(int restaurantId, int status);
 	
 	public List<Branch> listBranchByRestaurantId(int restaurantId);
 }
