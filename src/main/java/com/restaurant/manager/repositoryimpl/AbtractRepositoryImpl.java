085c472f464486a2f96cc039a019df71abf05782
2uocbao <2uocbao.IT@gmail.com>
2023-03-09 16:46:37 +0700

    Update AbtractRepositoryImpl.java
    update method call query, get list, call query for entity, call query for list entity.

--- src/main/java/com/restaurant/manager/repositoryimpl/AbtractRepositoryImpl.java
+++ src/main/java/com/restaurant/manager/repositoryimpl/AbtractRepositoryImpl.java
@@ -1,6 +1,9 @@
 package com.restaurant.manager.repositoryimpl;
 
 import java.io.Serializable;
+import java.lang.reflect.ParameterizedType;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -9,13 +12,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 public abstract class AbtractRepositoryImpl<T extends Serializable> {
 
-	private Class<T> c;
-	
-	Transaction transaction = null;
-	Session session = null;
+	private Class<T> entityClass;
+
+	Transaction transaction;
+	Session session;
 	@Autowired
 	private SessionFactory sessionFactory;
-	
+
+	@SuppressWarnings("unchecked")
+	protected AbtractRepositoryImpl() {
+		this.entityClass = (Class<T>) ((ParameterizedType) this.getClass().getGenericSuperclass())
+				.getActualTypeArguments()[0];
+	}
+
 	public T create(T entity) {
 		try {
 			session = sessionFactory.openSession();
@@ -33,7 +42,7 @@ public abstract class AbtractRepositoryImpl<T extends Serializable> {
 		}
 		return entity;
 	}
-	
+
 	public T update(T entity) {
 		try {
 			session = sessionFactory.openSession();
@@ -51,13 +60,13 @@ public abstract class AbtractRepositoryImpl<T extends Serializable> {
 		}
 		return entity;
 	}
-	
+
 	public T getDetail(int id) {
 		T entity = null;
 		try {
 			session = sessionFactory.openSession();
 			transaction = session.beginTransaction();
-			entity = session.get(c, id);
+			entity = session.get(this.entityClass, id);
 			transaction.commit();
 		} catch (Exception e) {
 			if (transaction != null) {
@@ -69,5 +78,65 @@ public abstract class AbtractRepositoryImpl<T extends Serializable> {
 				session.close();
 		}
 		return entity;
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<T> getList() {
+		List<T> ts = new ArrayList<>();
+		try {
+			session = sessionFactory.openSession();
+			transaction = session.beginTransaction();
+			ts = session.createQuery("From" + entityClass.getName()).list();
+			transaction.commit();
+		} catch (Exception e) {
+			if (transaction != null) {
+				transaction.rollback();
+			}
+			e.printStackTrace();
+		} finally {
+			if (session.isOpen())
+				session.close();
+		}
+		return ts;
+	}
+
+	@SuppressWarnings("unchecked")
+	public T callQueryForEntity(String query) {
+		T entity = null;
+		try {
+			session = sessionFactory.openSession();
+			transaction = session.beginTransaction();
+			entity = (T) session.createQuery(query).uniqueResult();
+			transaction.commit();
+		} catch (Exception e) {
+			if (transaction != null) {
+				transaction.rollback();
+			}
+			e.printStackTrace();
+		} finally {
+			if (session.isOpen())
+				session.close();
+		}
+		return entity;
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<T> callQueryForListEntity(String query) {
+		List<T> ts = new ArrayList<>();
+		try {
+			session = sessionFactory.openSession();
+			transaction = session.beginTransaction();
+			ts = session.createQuery(query).list();
+			transaction.commit();
+		} catch (Exception e) {
+			if (transaction != null) {
+				transaction.rollback();
+			}
+			e.printStackTrace();
+		} finally {
+			if (session.isOpen())
+				session.close();
+		}
+		return ts;
 	}
 }
