2233439af531f21a6edfdb2cb0693a95470188cb
2uocbao <2uocbao.IT@gmail.com>
2023-07-06 20:15:33 +0700

    Update JwtAuthenticationFilter.java

--- src/main/java/com/restaurant/manager/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/restaurant/manager/jwt/JwtAuthenticationFilter.java
@@ -9,27 +9,26 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import com.restaurant.manager.service.CustomUserDetailService;
+import lombok.AllArgsConstructor;
 
+@Component
+@AllArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(JwtAuthenticationFilter.class);
 
-	@Autowired
-	JwtTokenProvider jwtTokenProvider;
+	private final JwtTokenProvider jwtTokenProvider;
+	private final UserDetailsService customUserDetailService;
 
-	@Autowired
-	CustomUserDetailService customUserDetailService;
-
-	
 	// Only one call for a given request
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
@@ -37,9 +36,9 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 		try {
 			String jwt = getJwtFromRequest(request);
 			if (StringUtils.hasText(jwt) && jwtTokenProvider.validateToken(jwt)) {
-				int userId = jwtTokenProvider.getUserIdFromJWT(jwt);
+				String username = jwtTokenProvider.getUserNameFromJWT(jwt);
 
-				UserDetails userDetails = customUserDetailService.loadUserById(userId);
+				UserDetails userDetails = customUserDetailService.loadUserByUsername(username);
 
 				UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
 						userDetails, null, userDetails.getAuthorities());
