dfcce951d0a2294324a73710bc76f158e6653626
2uocbao <2uocbao.IT@gmail.com>
2023-07-06 20:33:05 +0700

    Update CustomUserDetailService.java

--- src/main/java/com/restaurant/manager/service/CustomUserDetailService.java
+++ src/main/java/com/restaurant/manager/service/CustomUserDetailService.java
@@ -2,6 +2,10 @@ package com.restaurant.manager.service;
 
 import org.springframework.security.core.userdetails.UserDetails;
 
+import com.restaurant.manager.request.LoginRequest;
+
 public interface CustomUserDetailService {
 	UserDetails loadUserById(int id);
+	
+	public String login(LoginRequest loginRequest);
 }
