52c4260eb428918f9aee64f219e1b59e4c3e06fc
2uocbao <2uocbao.IT@gmail.com>
2023-07-06 20:08:19 +0700

    Update AuthenController.java

--- src/main/java/com/restaurant/manager/controller/AuthenController.java
+++ src/main/java/com/restaurant/manager/controller/AuthenController.java
@@ -5,36 +5,23 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.restaurant.manager.jwt.JwtTokenProvider;
 import com.restaurant.manager.request.LoginRequest;
+import com.restaurant.manager.service.CustomUserDetailService;
 
 @RestController
 @RequestMapping("/authen")
 public class AuthenController {
 
 	@Autowired
-	AuthenticationManager authenticationManager;
-
-	@Autowired
-	JwtTokenProvider jwtTokenProvider;
+	CustomUserDetailService customUserDetailService;
 
 	@PostMapping("/singin")
 	ResponseEntity<String> singinUser(@Valid @RequestBody LoginRequest loginRequest) {
-		// check information user do valid
-		Authentication authentication = authenticationManager.authenticate(
-				new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
-		// Set information from authentication to Security Context
-		SecurityContextHolder.getContext().setAuthentication(authentication);
-		String jwt = jwtTokenProvider.generateToken(authentication);
-		return ResponseEntity.status(HttpStatus.OK).body(jwt);
+		return ResponseEntity.status(HttpStatus.OK).body(customUserDetailService.login(loginRequest));
 	}
 }
